7cc7631839136ba038d33330a8eb639772204275
lim yi xuan <limyx1215@hotmail.com>
2023-08-05 15:27:18 +0800

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "list", to: "tasks#list", as: "tasks"
+  get "list/new", to: "tasks#new", as: "new_task"
+  post "list", to: "tasks#create"
+  get "list/:id", to: "tasks#show", as: "show_task"
+  get "list/:id/edit", to: "tasks#edit"
+  patch "list/:id/edit", to: "tasks#update", as: "update_task"
+  delete "list/:id", to: "tasks#destroy", as: "destroy_task"
 end
